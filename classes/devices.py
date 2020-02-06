08e55e06703af94d9a5b3e549372ff6ca6c00a33
fredrikj31 <fredrik1206@gmail.com>
2020-02-06 08:45:47 +0100

    Removed and added auth to api

--- classes/devices.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_restful import Resource, Api, reqparse
-
-class Device(Resource):
-	def get(self):
-		# If the key does not exist in the data store, return a 404 error.
-
-		return {'message': 'Device found', 'data': "Hej med dig"}, 200
--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+from flask_httpauth import HTTPBasicAuth
 import os
 import markdown
-
-#Importing Classes
-from classes import devices
-
 
 app = Flask(__name__)
 
 api = Api(app)
 
-api.add_resource(devices.Device, '/devices')
+auth = HTTPBasicAuth()
+
+USER_DATA = {
+    "admin": "SecretPassword"
+}
+
+@auth.verify_password
+def verify(username, password):
+    if not (username and password):
+        return False
+    return USER_DATA.get(username) == password
+
+class Device(Resource):
+	@auth.login_required
+	def get(self):
+		# If the key does not exist in the data store, return a 404 error.
+
+		return {'message': 'Device found', 'data': "Hej med dig"}, 200
 
 @app.route("/")
 def index():
@@ -22,4 +36,7 @@ def index():
 
 		return content
 
-app.run(host='0.0.0.0', port='80', debug=True)
+
+api.add_resource(Device, '/devices')
+
+app.run(host='127.0.0.1', port='5000', debug=True)
